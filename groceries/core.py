bc2bcadeb58b8593f5b281bbc1ccc4bb702a4680
Teun Zengerink <t.zengerink@gmail.com>
2015-08-26 16:24:32 +0200

    implemented login command

--- groceries/core.py
+++ groceries/core.py
@@ -1,16 +1,22 @@
 import click
+import json
+import requests
+import os
 import yaml
-
-
-CONFIG = dict(api='http://localhost',
-              auth=dict(username='user', password='pass'))
 
 
 @click.group()
 @click.option('-c', '--config', default='.groceries.yml', type=click.File())
-def cli(config):
+@click.pass_context
+def cli(ctx, config):
     '''A command line interface for the Groceries API.'''
-    CONFIG = yaml.load(config.read())
+    try:
+        with os.fdopen(os.open('.token', os.O_RDONLY), 'r') as file:
+            token = file.readline()
+    except:
+        token = ''
+    ctx.obj = {'CONFIG': yaml.load(config.read()),
+               'TOKEN': token}
 
 
 @cli.command()
@@ -35,3 +41,20 @@ def list():
 def remove():
     '''Remove an item from the groceries list.'''
     click.echo('Removing grocery...')
+
+
+@cli.command()
+@click.option('-u', '--username', prompt=True)
+@click.option('-p', '--password', prompt=True, hide_input=True)
+@click.pass_context
+def login(ctx, username, password):
+    '''Authenticate using a username and password.'''
+    r = requests.post('{0}{1}'.format(ctx.obj['CONFIG'].get('api'), '/login'),
+                      data=json.dumps(dict(username=username, password=password)),
+                      headers={'Content-Type':'application/json'})
+    if r.status_code is not 200:
+        click.echo('Login failed')
+        ctx.exit(1)
+    with os.fdopen(os.open('.token', os.O_WRONLY | os.O_CREAT, 0600), 'w') as file:
+        file.write(r.json().get('token'))
+    click.echo('Login successfull')
--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     install_requires=[
         'Click',
         'PyYaml',
+        'requests',
     ],
     entry_points='''
         [console_scripts]
