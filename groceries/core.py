c4fd3d710940926c0a139f5e84630e9a9cfc43e1
Teun Zengerink <t.zengerink@gmail.com>
2015-08-27 14:58:38 +0200

    added config generation wizard

--- groceries/core.py
+++ groceries/core.py
@@ -1,4 +1,6 @@
 import click
+import os
+import requests
 import yaml
 
 from functools import update_wrapper
@@ -15,8 +17,39 @@ def authenticated(f):
     return update_wrapper(decorated_function, f)
 
 
+def validate_config(ctx, param, values, tries=0):
+    '''Validates if config file exists, if not create a configuration file.
+
+    Args:
+        ctx: click.Context
+        param: click.Param
+        values: parameter value
+        tries: number of tries
+
+    Returns:
+        Open file object
+    '''
+    if not os.path.exists(values):
+        if tries == 0:
+            click.echo('No config file found. Let\'s create it:')
+        url = raw_input('What is the API\'s base URL? ')
+        try:
+            if not requests.get(url).status_code != 200:
+                raise Exception()
+        except:
+            tries += 1
+            if tries < 3:
+                click.echo('Invalid URL supplied')
+                return validate_config(ctx, param, values, tries)
+            click.echo('To many tries')
+            ctx.exit(1)
+        with os.fdopen(os.open(values, os.O_WRONLY | os.O_CREAT, 0600), 'w') as file:
+            file.write('api: {0}'.format(url))
+    return os.fdopen(os.open(values, os.O_RDONLY), 'r')
+
+
 @click.group()
-@click.option('-c', '--config', default='.groceries.yml', type=click.File())
+@click.option('-c', '--config', default='.groceries.yml', callback=validate_config)
 @click.pass_context
 def cli(ctx, config):
     '''A command line interface for the Groceries API.
