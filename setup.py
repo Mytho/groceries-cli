e41cbfe495d495a6ddcff13efcb1223123a93547
Teun Zengerink <t.zengerink@gmail.com>
2015-08-28 17:55:50 +0200

    added coveralls

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     install_requires=[
         'click==5.1',
         'coverage==3.7.1',
+        'coveralls==0.5',
         'flake8==2.4.1',
         'mock==1.3.0',
         'pytest==2.7.2',
