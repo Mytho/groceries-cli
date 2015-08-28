d2c1d812f10a0d3d1d63028c113efab95a91fff2
Teun Zengerink <t.zengerink@gmail.com>
2015-08-28 14:51:01 +0200

    extracted steps from the wizard

--- groceries/config.py
+++ groceries/config.py
@@ -9,35 +9,60 @@ import yaml
 class Config(object):
 
     def __init__(self, path):
+        '''Create a new instance.
+
+        Args:
+            path: string containing file path
+        '''
         self.path = path
         self.read()
 
     def get(self, key):
+        '''Get a configuration value.
+
+        Args:
+            key: key as a string to get value for
+
+        Returns:
+            Configuration value for requested key.
+        '''
         if not self.yaml:
             return None
         return self.yaml.get(key)
 
     def read(self):
+        '''Read the contents of the configuration file.'''
         with os.fdopen(os.open(self.path, os.O_RDONLY | os.O_CREAT, 0600), 'r') as file:
             y = yaml.load(file.read())
             self.yaml = y if y else {}
 
     def set(self, key, value):
+        '''Set a configuration value.
+
+        Args:
+            key: key as a string to set the value for
+            value: value to assign to key
+        '''
         if not self.yaml:
             self.yaml = {}
         self.yaml[key] = value
 
     def write(self):
+        '''Write the configuration to the file.'''
         with os.fdopen(os.open(self.path, os.O_WRONLY | os.O_CREAT, 0600), 'w') as file:
             file.write(yaml.safe_dump(self.yaml, default_flow_style=False))
 
 
 class Wizard(object):
 
-    max_tries = 3
+    show_message = True
+
+    def __init__(self):
+        '''Helps create a configuration file when it does not exist.'''
+        self.steps = [Api(), Token()]
 
     def __call__(self, ctx, param, values):
-        '''Helps create a configuration file when it does not exist.
+        '''Usable as the callback in a click.Parameter.
 
         Args:
             ctx: click.Context
@@ -48,46 +73,87 @@ class Wizard(object):
             groceries.config.Config
         '''
         config = Config(values)
-        if not config.get('api'):
-            config = self.api(ctx, config)
-        if not config.get('token'):
-            config = self.token(ctx, config)
+        for step in self.steps:
+            if not config.get(step.key):
+                if self.show_message:
+                    self.show_message = False
+                    click.echo('Part of the configuration is incomplete. '
+                               'Please answer the following questions:')
+                config = step(ctx, config)
         return config
 
-    def api(self, ctx, config, count=1):
-        if count == 1:
-            click.echo('Config is missing an API setting.')
-        url = raw_input('What is the URL of the API? ')
-        try:
-            if not requests.get('{0}{1}'.format(url, '/status')).status_code == 200:
-                raise Exception()
-            config.set('api', url)
-            config.write()
-        except Exception, e:
-            if count < self.max_tries:
-                click.echo('Incorrect URL provided')
-                count += 1
-                return self.api(ctx, config, count)
-            ctx.abort()
-        return config
 
-    def token(self, ctx, config, count=1):
-        if count == 1:
-            click.echo('Configuration is missing a token.')
-        username = raw_input('What is your username? ')
-        password = getpass.getpass('And your password? ')
+class Step(object):
+
+    key = None
+    error = 'Invalid value provided'
+    max_tries = 3
+
+    def __call__(self, ctx, config, count=1):
+        '''Execute a single step in the wizard.
+
+        Args:
+            ctx: click.Context
+            config: groceries.config.Config
+            count: number of tries
+
+        Returns:
+            groceries.config.Config
+        '''
         try:
-            token = requests.post('{0}{1}'.format(config.get('api'), '/login'),
-                                  data=json.dumps({'username': username, 'password': password}),
-                                  headers={'Content-Type': 'application/json'}).json().get('token')
-            if not token:
-                raise Exception()
-            config.set('token', token)
-            config.write()
+            config.set(self.key, self.value(config))
         except:
             if count < self.max_tries:
-                click.echo('Invalid username and/or password.')
+                click.echo(self.error)
                 count += 1
-                return self.token(ctx, config, count)
+                return self(ctx, config, count)
             ctx.abort()
+        config.write()
         return config
+
+    def value(self):
+        raise NotImplemented()
+
+
+class Api(Step):
+
+    key = 'api'
+    error = 'Invalid URL provided'
+
+    def value(self, config):
+        '''Get the URL for the API.
+
+        Args:
+            config: groceries.config.Config
+
+        Returns:
+            String containing the URL.
+        '''
+        url = raw_input('What is the URL of the API? ')
+        if not requests.get('{0}{1}'.format(url, '/status')).status_code == 200:
+            raise Exception()
+        return url
+
+
+class Token(Step):
+
+    key = 'token'
+    error = 'Invalid username and/or password provided.'
+
+    def value(self, config):
+        '''Get the token to use for authentication.
+
+        Args:
+            config: groceries.config.Config
+
+        Returns:
+            String containing the token.
+        '''
+        username = raw_input('What is your username? ')
+        password = getpass.getpass('And your password? ')
+        token = requests.post('{0}{1}'.format(config.get('api'), '/login'),
+                              data=json.dumps({'username': username, 'password': password}),
+                              headers={'Content-Type': 'application/json'}).json().get('token')
+        if not token:
+            raise Exception()
+        return token
